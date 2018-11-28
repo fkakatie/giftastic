4d805376d8385e7847b03d649718739ab67a7143
fkakatie <sextonskatie@gmail.com>
2018-11-27 21:41:10 -0700

    messy but functional

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,24 +1,76 @@
-var btnOptions = ["the office", "b99", "superstore", "community"];
+var topics = ["the office", "b99", "superstore", "community"];
+
+var giphyAPI = "bjggJV6O96rSqs1w9A4ivSwF6NFLgGTY";
+
+var offset = 0;
 
 $(document).ready(function() {
 
-    // for-loop iterates through the btnOptions array, creating button for each item in array
-    for (var i = 0; i < btnOptions.length; i++) {
+    // for-loop iterates through the topics array, creating button for each item in array
+    for (var i = 0; i < topics.length; i++) {
 
-        var btnOptionsPlus = btnOptions[i].split(' ').join('+');
+        var topicsPlus = topics[i].split(' ').join('+');
 
         var btn = $('<button>');
         btn.addClass('btnClass');
-        btn.attr('data-option', btnOptionsPlus);
-        btn.text(btnOptions[i]);
+        btn.attr('data-topic', topicsPlus);
+        btn.text(topics[i]);
         $('#buttons').append(btn);
 
     };
 
     $('.btnClass').on('click', function() {
 
-        console.log($(this).attr('data-option'));
+        var searchTerm = $(this).attr('data-topic');
+
+        $.ajax({
+            url: "http://api.giphy.com/v1/gifs/search?q=" + searchTerm + "&api_key=" + giphyAPI + "&offset=" + offset + "&limit=5",
+            method: "GET"
+        }).then(function(response) {
+    
+            for (var j = 0; j < response.data.length; j++) {
+    
+                var giphy = $('<img>');
+                giphy.addClass('gifClass');
+                giphy.attr('src', response.data[j].images.fixed_width_still.url);
+                $('#gifs').prepend(giphy);
+     
+            }
+    
+        });
+
+        offset += 5;
 
     });
 
-})
+    $('#gifs').on('click', 'img.gifClass', function() {
+
+        console.log('this works!');
+
+        var URL = $(this).attr('src');
+
+        if (URL.includes('_s')) {
+            $(this).attr('src', URL.replace('_s',''));
+        }
+        else {
+            $(this).attr('src', URL.replace('.gif','_s.gif'));
+        }
+
+    });
+
+    $('button').click(function() {
+        if ($('.top').attr('colspan')) {
+            $('.top').removeAttr('colspan');
+        } else {
+            $('.top').attr('colspan', 6);
+        }
+    });
+
+   
+    $('#clear').on('click', function() {
+
+        $('#gifs').empty();
+
+    });
+
+});
