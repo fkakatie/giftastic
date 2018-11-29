14bbdcf9d4915bd02715cc44f02fd9eb26c5b85f
fkakatie <sextonskatie@gmail.com>
2018-11-28 20:35:34 -0700

    some basic styling

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,30 +1,35 @@
 var topics = ["the office", "b99", "superstore", "community", "the good place"];
 
-var giphyAPI = "bjggJV6O96rSqs1w9A4ivSwF6NFLgGTY";
+var giphyAPI = "bjggJV6O96rSqs1w9A4ivSwF6NFLgGTY"; // my giphy api key
 
-var offset = 0;
+var offset = 0; // sets offset so new gifs are reloaded
 
+// wait for dom to load
 $(document).ready(function() {
 
     // for-loop iterates through the topics array, creating button for each item in array
     for (var i = 0; i < topics.length; i++) {
 
+        // replace spaces in topic with plus signs
         var topicsPlus = topics[i].split(' ').join('+');
 
+        // create buttons
         var btn = $('<button>');
         btn.addClass('btnClass');
-        btn.attr('data-topic', topicsPlus);
+        btn.attr('data-topic', topicsPlus); // add data-topic, will become our search term
         btn.text(topics[i]);
         $('#buttons').append(btn);
 
     };
 
+    // load gifs on button click
     $('.btnClass').on('click', function() {
 
+        // grab data-topic (topic with plus signs instead of spaces)
         var searchTerm = $(this).attr('data-topic');
 
         $.ajax({
-            url: "https://api.giphy.com/v1/gifs/search?q=" + searchTerm + "&api_key=" + giphyAPI + "&offset=" + offset + "&limit=5",
+            url: "https://api.giphy.com/v1/gifs/search?q=" + searchTerm + "&api_key=" + giphyAPI + "&offset=" + offset + "&limit=10",
             method: "GET"
         }).then(function(response) {
     
@@ -39,7 +44,7 @@ $(document).ready(function() {
     
         });
 
-        offset += 5;
+        offset += 10;
 
     });
 
